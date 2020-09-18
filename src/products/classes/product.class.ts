d940e8b28b422b80b50689410028054ec3c15655
Jorge Chable <yahicimosclick9193@gmail.com>
2020-09-18 17:37:21 -0500

    Swagger - Definición de response productos

---
+++ src/products/classes/product.class.ts
@@ -0,0 +1,69 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { v4 as uuid } from 'uuid';
+import {
+  ProductCategories,
+  ProductQuantities,
+  ProductStatus,
+} from './combos.class';
+
+export class Product {
+  @ApiProperty({
+    description: 'Id',
+    example: '5f6198cfafd07022c4d47a9f',
+  })
+  _id: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Id del producto',
+    example: uuid(),
+  })
+  IdProduct: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Nombre del producto',
+    example: 'Producto 1',
+  })
+  readonly NameProduct: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Categoría del producto',
+    enum: ProductCategories,
+    default: 'Bebidas',
+  })
+  readonly Category: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Descripción del producto',
+    example: 'Generado desde la API',
+  })
+  readonly Description: string;
+
+  @ApiProperty({
+    type: Number,
+    description: 'Cantidad de producto',
+    minimum: 1,
+    maximum: 100,
+    enum: ProductQuantities,
+    default: 1,
+  })
+  readonly ProductQuantity: number;
+
+  @ApiProperty({
+    type: Boolean,
+    description: 'Estatus del producto',
+    enum: ProductStatus,
+    default: true,
+  })
+  readonly Status: boolean;
+
+  @ApiProperty({
+    type: Number,
+    description: 'Fecha y hora de creación',
+    example: Math.floor(new Date().getTime() / 1000),
+  })
+  TimeStamp: number;
+}
